a69163bf95d54bf597ad20fbdad1192a4b457947
Rashad Edwards <rashadedwards@macbook.local>
2024-05-25 07:06:45 -0400

    updated commit

--- breakfastMenu/breakfastMenu.js
+++ breakfastMenu/breakfastMenu.js
@@ -1,11 +1,34 @@
 let menu = document.querySelectorAll(".menu");
 const breakfast = document.querySelector("#breakfast");
-const lunch = document.querySelector("#lunch");
-const dinner = document.querySelector("#dinner");
-const dessert = document.querySelector("#dessert");
 const overlay = document.querySelector(".overlay");
 const breakfastElement = document.createElement("div");
 
+// Breakfast Menu
+const breakfastMenu = [
+  {
+    item: "Eggs Sandwich",
+    price: 12,
+    description: "Two over easy fried eggs, between two buttered toast.",
+  },
+  {
+    item: "Eggs Benedict",
+    price: 13,
+    description:
+      "English Muffin cut in half, bacon, poached egg, and hollandaise sauce.",
+  },
+  {
+    item: "Omelet",
+    price: 15,
+    description: "Folded eggs, tossed with veggies, salsa, meat of choice.",
+  },
+  {
+    item: "Breakfast Tacos",
+    price: 14,
+    description: "Eggs, bacon, diced potatoes, with salsa.",
+  },
+];
+
+// Overlay for each menu Item
 const menuChoices = () => {
   for (let i = 0; i < menu.length; i++) {
     menu[i].onclick = function (e) {
@@ -17,29 +40,26 @@ const menuChoices = () => {
   }
 };
 
-const breakfastMenu = [
-  {
-    item: "Eggs Sandwich",
-    price: 7,
-    description: "Toasted Bread, with Egg and Cheese",
-  },
-  {
-    item: "Cinnamon Rolls",
-    price: 10,
-    description: "Dessert",
-  },
-  {
-    item: "Eggs Benny",
-    price: 7,
-    description: "Toasted Bread, with Egg and Cheese",
-  },
-  {
-    item: "Omelet",
-    price: 10,
-    description: "Omelet",
-  },
-];
+// overlay for modal to close display
+const closeOverlay = () => {
+  overlay.classList.add("hidden");
+  breakfastElement.remove();
+};
 
+// Create an div element after breakfast Id in html has been selected set that div id to 'break-style'
+// Check Css is for styling of break style
+const breakfastMenuDisplay = () => {
+  breakfast.addEventListener("click", () => {
+    breakfast.after(breakfastElement);
+    breakfastElement.setAttribute("id", "break-style");
+  });
+};
+
+const breakfastMenuCloseModal = () => {
+  overlay.addEventListener("click", closeOverlay);
+};
+
+// Using innerHtml to render breakfast menu from objects on page
 const getBreakfastItem = () => {
   breakfastElement.innerHTML += `
 
@@ -57,33 +77,33 @@ const getBreakfastItem = () => {
         <div>
           <div id="break-style"> ${breakfastMenu[0].item}
         <span class="break-span">
-          Price: ${breakfastMenu[0].price}
+          Price: $${breakfastMenu[0].price}
         </span>
         <div class="description">
-          Description: ${breakfastMenu[0].description}
+           ${breakfastMenu[0].description}
         </div>
           <div id="break-style2" > ${breakfastMenu[1].item}
         <span class="break-span">
-          Price: ${breakfastMenu[1].price}
+          Price: $${breakfastMenu[1].price}
         </span>
         <div class="description">
-          Description: ${breakfastMenu[1].description}
+           ${breakfastMenu[1].description}
         </div>
         </div>
           <div id="break-style2" > ${breakfastMenu[2].item}
         <span class="break-span">
-          Price: ${breakfastMenu[2].price}
+          Price: $${breakfastMenu[2].price}
         </span>
         <div class="description">
-          Description: ${breakfastMenu[2].description}
+            ${breakfastMenu[2].description}
         </div>
         </div>
           <div id="break-style2" > ${breakfastMenu[3].item}
         <span class="break-span">
-          Price: ${breakfastMenu[3].price}
+          Price: $${breakfastMenu[3].price}
         </span>
         <div class="description">
-          Description: ${breakfastMenu[3].description}
+          ${breakfastMenu[3].description}
         </div>
       </section>
       </div>
@@ -92,31 +112,6 @@ const getBreakfastItem = () => {
     `;
 };
 
-const breakfastMenuOpenModal = () => {
-  breakfast.addEventListener("click", () => openModal);
-};
-
-const breakfastMenuDisplay = () => {
-  breakfast.addEventListener("click", () => {
-    breakfast.after(breakfastElement);
-    breakfastElement.setAttribute("id", "break-style");
-  });
-};
-
-const openModal = () => {
-  overlay.classList.remove("hidden");
-};
-
-const closeModal = () => {
-  overlay.classList.add("hidden");
-  breakfastElement.remove();
-};
-
-const breakfastMenuCloseModal = () => {
-  overlay.addEventListener("click", closeModal);
-};
-
-breakfastMenuOpenModal();
 breakfastMenuCloseModal();
 breakfastMenuDisplay();
 getBreakfastItem();
