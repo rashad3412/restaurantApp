509cbc8e5715351c1c3318c7db0d9239fd486a98
Rashad Edwards <rashadedwards@macbook.local>
2024-05-26 23:25:14 -0400

    updated commit

---
+++ dessertMenu/dessertMenu.js
@@ -0,0 +1,133 @@
+const menu4 = document.querySelectorAll(".menu");
+const dessert = document.querySelector("#dessert");
+const dessertOverlay = document.querySelector(".overlay");
+const dessertElement = document.createElement("div");
+
+// Breakfast Menu
+const dessertMenu = [
+  {
+    item: "Ice Cream",
+    price: 10,
+    description:
+      "Different flavor ice creams, vanilla, strawberry, chocolate chip.",
+  },
+  {
+    item: "Strawberry Crunch Cheese Cakes",
+    price: 12,
+    description:
+      "Strawberry Moose, gram craker crust, topped with strawberry glaze.",
+  },
+  {
+    item: "Churros",
+    price: 8,
+    description: " Cinnamon Sugar churros.",
+  },
+  {
+    item: "Cookies",
+    price: 5,
+    description: "Caramel butter cookies.",
+  },
+  {
+    item: "Carrot Cake",
+    price: 13,
+    description: "Savory cake, with carrots and cream cheese toppings.",
+  },
+];
+
+// Overlay for each menu Item
+const dessertMenuChoices = () => {
+  for (let i = 0; i < menu4.length; i++) {
+    menu4[i].onclick = function (e) {
+      e.preventDefault();
+      menu4[i] = document.querySelector(e.target.getAttribute("href"));
+      menu4[i].classList.remove("hidden");
+      dessertOverlay.classList.remove("hidden");
+    };
+  }
+};
+
+// overlay for modal to close display
+const closeDessertOverlay = () => {
+  dessertOverlay.classList.add("hidden");
+  dessertElement.remove();
+};
+
+// Create an div element after breakfast Id in html has been selected set that div id to 'break-style'
+// Check Css is for styling of break style
+const dessertMenuDisplay = () => {
+  dessert.addEventListener("click", () => {
+    dessert.after(dessertElement);
+    dessertElement.setAttribute("id", "break-style");
+  });
+};
+
+const dessertMenuCloseModal = () => {
+  dessertOverlay.addEventListener("click", closeDessertOverlay);
+};
+
+// Using innerHtml to render breakfast menu from objects on page
+const getDessertItem = () => {
+  dessertElement.innerHTML += `
+
+  <div id="center-pic">
+
+  <img id="menu-pic"  src="https://img.freepik.com/premium-photo/petunia-hand-drawn-black-white-sketch-petunia-flower-isolated-white-background-vector-illustration_555437-202.jpg?w=2000" />
+
+  </div>
+  <h1 id="break-h1">Dessert Menu</h1>
+  <h3 id="break-h3">MeezVibez</h3>
+
+  <div id="break-obj>
+    <div>
+      <section>
+        <div>
+          <div id="break-style"> ${dessertMenu[0].item}
+        <span class="break-span">
+          Price: $${dessertMenu[0].price}
+        </span>
+        <div class="description">
+           ${dessertMenu[0].description}
+        </div>
+          <div id="break-style2" > ${dessertMenu[1].item}
+        <span class="break-span">
+          Price: $${dessertMenu[1].price}
+        </span>
+        <div class="description">
+           ${dessertMenu[1].description}
+        </div>
+        </div>
+          <div id="break-style2" > ${dessertMenu[2].item}
+        <span class="break-span">
+          Price: $${dessertMenu[2].price}
+        </span>
+        <div class="description">
+            ${dessertMenu[2].description}
+        </div>
+        </div>
+          <div id="break-style2" > ${dessertMenu[3].item}
+        <span class="break-span">
+          Price: $${dessertMenu[3].price}
+        </span>
+        <div class="description">
+          ${dessertMenu[3].description}
+        </div>
+        </div>
+          <div id="break-style2" > ${dessertMenu[4].item}
+        <span class="break-span">
+          Price: $${dessertMenu[4].price}
+        </span>
+        <div class="description">
+          ${dessertMenu[4].description}
+        </div>
+      </section>
+      </div>
+    </div>
+      </div>
+    `;
+};
+
+closeDessertOverlay();
+dessertMenuCloseModal();
+dessertMenuDisplay();
+getDessertItem();
+dessertMenuChoices();
---
+++ dinnerMenu/dinnerMenu.js
@@ -0,0 +1,131 @@
+const menu3 = document.querySelectorAll(".menu");
+const dinner = document.querySelector("#dinner");
+const dinnerOverlay = document.querySelector(".overlay");
+const dinnerElement = document.createElement("div");
+
+// Breakfast Menu
+const dinnerMenu = [
+  {
+    item: "Crab Cakes",
+    price: 25,
+    description: "Seared crab cakes, yellow rice, garlic butter sauce.",
+  },
+  {
+    item: "Ribeye",
+    price: 40,
+    description: "Angus beef, brussel sprouts, and red potatoes.",
+  },
+  {
+    item: "Mahi Mahi",
+    price: 23,
+    description: " yellow rice and seafood slaw.",
+  },
+  {
+    item: "Lobster tail with Mussles",
+    price: 35,
+    description: "Lobster tail, mussles, pasta.",
+  },
+  {
+    item: "Clams and Lobser bites",
+    price: 32,
+    description: "Clams, pasta and cube lobster meat.",
+  },
+];
+
+// Overlay for each menu Item
+const dinnerMenuChoices = () => {
+  for (let i = 0; i < menu3.length; i++) {
+    menu3[i].onclick = function (e) {
+      e.preventDefault();
+      menu3[i] = document.querySelector(e.target.getAttribute("href"));
+      menu3[i].classList.remove("hidden");
+      dinnerOverlay.classList.remove("hidden");
+    };
+  }
+};
+
+// overlay for modal to close display
+const closeDinnerOverlay = () => {
+  dinnerOverlay.classList.add("hidden");
+  dinnerElement.remove();
+};
+
+// Create an div element after breakfast Id in html has been selected set that div id to 'break-style'
+// Check Css is for styling of break style
+const dinnerMenuDisplay = () => {
+  dinner.addEventListener("click", () => {
+    dinner.after(dinnerElement);
+    dinnerElement.setAttribute("id", "break-style");
+  });
+};
+
+const dinnerMenuCloseModal = () => {
+  dinnerOverlay.addEventListener("click", closeDinnerOverlay);
+};
+
+// Using innerHtml to render breakfast menu from objects on page
+const getDinnerItem = () => {
+  dinnerElement.innerHTML += `
+
+  <div id="center-pic">
+
+  <img id="menu-pic"  src="https://img.freepik.com/premium-photo/petunia-hand-drawn-black-white-sketch-petunia-flower-isolated-white-background-vector-illustration_555437-202.jpg?w=2000" />
+
+  </div>
+  <h1 id="break-h1">Dinner Menu</h1>
+  <h3 id="break-h3">MeezVibez</h3>
+
+  <div id="break-obj>
+    <div>
+      <section>
+        <div>
+          <div id="break-style"> ${dinnerMenu[0].item}
+        <span class="break-span">
+          Price: $${dinnerMenu[0].price}
+        </span>
+        <div class="description">
+           ${dinnerMenu[0].description}
+        </div>
+          <div id="break-style2" > ${dinnerMenu[1].item}
+        <span class="break-span">
+          Price: $${dinnerMenu[1].price}
+        </span>
+        <div class="description">
+           ${dinnerMenu[1].description}
+        </div>
+        </div>
+          <div id="break-style2" > ${dinnerMenu[2].item}
+        <span class="break-span">
+          Price: $${dinnerMenu[2].price}
+        </span>
+        <div class="description">
+            ${dinnerMenu[2].description}
+        </div>
+        </div>
+          <div id="break-style2" > ${dinnerMenu[3].item}
+        <span class="break-span">
+          Price: $${dinnerMenu[3].price}
+        </span>
+        <div class="description">
+          ${dinnerMenu[3].description}
+        </div>
+        </div>
+          <div id="break-style2" > ${dinnerMenu[4].item}
+        <span class="break-span">
+          Price: $${dinnerMenu[4].price}
+        </span>
+        <div class="description">
+          ${dinnerMenu[4].description}
+        </div>
+      </section>
+      </div>
+    </div>
+      </div>
+    `;
+};
+
+closeDinnerOverlay();
+dinnerMenuCloseModal();
+dinnerMenuDisplay();
+getDinnerItem();
+dinnerMenuChoices();
